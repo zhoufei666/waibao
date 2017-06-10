c073bfb932fdef1738d5743cc4e62979712b13a2
zhoufei <596740325@qq.com>
2017-06-11 03:11:54 +0800

    首页
    Signed-off-by: zhoufei <596740325@qq.com>

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -1,63 +1,141 @@
 @extends('Public.Home.common')
 
+
+@section('css')
+    <link rel="stylesheet" media="screen and (min-width:300px) and (max-device-width:800px)" href="{{asset('assets/Common/Css/index_min.css?v=3')}}"/>
+@stop
+
 @section('content')
-	<div id="contentleft">
-        <div class="m-post">
-            <h2 class="title"><a href="{{ url('article/index') }}">PHP 命名空间 解惑</a></h2>
-            <div class="info box">
-                <a class="date" href="{{ url('article/index') }}">2014-10-7</a>
+    <div class="img_box">
+        
+        <div class="img_item">
+            <div class="lightCon"> 
+                <span class="hoverBox"> 
+                    <img src="assets/Home/Image/we/1.jpg" title="我是大图片" alt="">
+                </span> 
+                <img class="basic_img" src="assets/Home/Image/we/1.jpg" alt=""> 
             </div>
         </div>
-        <div class="m-post">
-            <h2 class="title"><a href="{{ url('article/index') }}">JohnLui/AliyunOSS v1.0 发布，附 Laravel 框架详细使用教程及代码</a></h2>
-            <div class="info box">
-                <a class="date" href="{{ url('article/index') }}">2015-1-9</a>
+        <div class="img_item">
+            <div class="lightCon"> 
+                <span class="hoverBox"> 
+                    <img src="assets/Home/Image/we/2.jpg" title="我是大图片" alt="">
+                </span> 
+                <img class="basic_img" src="assets/Home/Image/we/2.jpg" alt=""> 
             </div>
         </div>
-        <div class="m-post">
-            <h2 class="title"><a href="https://lvwenhan.com/php/413.html">【完结】利用 Composer 完善自己的 PHP 框架（三）——Redis 缓存</a></h2>
-            <div class="info box">
-                <a class="date" href="https://lvwenhan.com/php/413.html">2014-10-19</a>
+        <div class="img_item">
+            <div class="lightCon"> 
+                <span class="hoverBox"> 
+                    <img src="assets/Home/Image/we/3.jpg" title="我是大图片" alt="">
+                </span> 
+                <img class="basic_img" src="assets/Home/Image/we/3.jpg" alt=""> 
             </div>
         </div>
-        <div class="m-post">
-            <h2 class="title"><a href="https://lvwenhan.com/php/412.html">利用 Composer 完善自己的 PHP 框架（二）——发送邮件</a></h2>
-            <div class="info box">
-                <a class="date" href="https://lvwenhan.com/php/412.html">2014-10-18</a>
+        <div class="img_item">
+            <div class="lightCon"> 
+                <span class="hoverBox"> 
+                    <img src="assets/Home/Image/we/4.jpg" title="我是大图片" alt="">
+                </span> 
+                <img class="basic_img" src="assets/Home/Image/we/4.jpg" alt=""> 
             </div>
         </div>
-        <div class="m-post">
-            <h2 class="title"><a href="https://lvwenhan.com/php/410.html">利用 Composer 完善自己的 PHP 框架（一）——视图装载</a></h2>
-            <div class="info box">
-                <a class="date" href="https://lvwenhan.com/php/410.html">2014-10-17</a>
+        <div class="img_item">
+            <div class="lightCon"> 
+                <span class="hoverBox"> 
+                    <img src="assets/Home/Image/we/5.jpg" title="我是大图片" alt="">
+                </span> 
+                <img class="basic_img" src="assets/Home/Image/we/5.jpg" alt=""> 
             </div>
         </div>
-        <div class="m-post">
-            <h2 class="title"><a href="https://lvwenhan.com/php/409.html">利用 Composer 一步一步构建自己的 PHP 框架（四）——使用 ORM</a></h2>
-            <div class="info box">
-                <a class="date" href="https://lvwenhan.com/php/409.html">2014-10-16</a>
+        <div class="img_item">
+            <div class="lightCon"> 
+                <span class="hoverBox"> 
+                    <img src="assets/Home/Image/we/6.jpg" title="我是大图片" alt="">
+                </span> 
+                <img class="basic_img" src="assets/Home/Image/we/6.jpg" alt=""> 
             </div>
         </div>
-        <div class="m-post">
-            <h2 class="title"><a href="https://lvwenhan.com/php/408.html">利用 Composer 一步一步构建自己的 PHP 框架（三）——设计 MVC</a></h2>
-            <div class="info box">
-                <a class="date" href="https://lvwenhan.com/php/408.html">2014-10-14</a>
+        <div class="img_item">
+            <div class="lightCon"> 
+                <span class="hoverBox"> 
+                    <img src="assets/Home/Image/we/7.jpg" title="我是大图片" alt="">
+                </span> 
+                <img class="basic_img" src="assets/Home/Image/we/7.jpg" alt=""> 
             </div>
         </div>
-        <div class="m-post">
-            <h2 class="title"><a href="https://lvwenhan.com/php/406.html">利用 Composer 一步一步构建自己的 PHP 框架（二）——构建路由</a></h2>
-            <div class="info box">
-                <a class="date" href="https://lvwenhan.com/php/406.html">2014-10-13</a>
+        <div class="img_item">
+            <div class="lightCon"> 
+                <span class="hoverBox"> 
+                    <img src="assets/Home/Image/we/5.jpg" title="我是大图片" alt="">
+                </span> 
+                <img class="basic_img" src="assets/Home/Image/we/birthday.jpg" alt=""> 
             </div>
         </div>
-        <div class="m-post">
-            <h2 class="title"><a href="https://lvwenhan.com/php/405.html">利用 Composer 一步一步构建自己的 PHP 框架（一）——基础准备</a></h2>
-            <div class="info box">
-                <a class="date" href="https://lvwenhan.com/php/405.html">2014-10-12</a>
+        <div class="img_item">
+            <div class="lightCon"> 
+                <span class="hoverBox"> 
+                    <img src="assets/Home/Image/we/5.jpg" title="喜欢，就去关注我的微信吧" alt="">
+                </span> 
+                <img class="basic_img" src="assets/Home/Image/wechat.jpg" alt=""> 
             </div>
         </div>
-        <div id="pagenavi">
+
+    </div>
+        
+
+
+    <!-- 小图片放大看大图及详细信息 -->
+    <div class="hoverBox_show">
+        <div class="hoverBox_show_img">
+                <div class="big_img">
+                    <div class=" big_img_box">
+                        <img src="" alt="">     
+                    </div>               
+                </div>
+
+                <div class="img_info">
+                    <span class="title">123</span>
+                    <span class="close">X</span>
+                </div>
         </div>
     </div>
-    <!-- end #contentleft-->
+
+
+
+@stop
+
+@section('script')
+    <script>
+        $(function(){
+            //点击显示大图片
+            $('.basic_img').click(function () {
+                var full_content_height = $('.full-content').outerHeight(true)+30; //整个画面div
+
+                var body_width = $('.full-content').width();
+
+                //赋值图片
+                var big_img_src = $(this).siblings('.hoverBox ').find('img').attr('src');
+                $('.big_img img').attr("src",big_img_src);
+                //赋值标题
+                var big_img_title =  $(this).siblings('.hoverBox ').find('img').attr('title');
+                $('.big_img img').attr('title',big_img_title);
+                
+                $('.hoverBox_show').show().css('height',full_content_height);//蒙版显示
+                
+                $('.big_img').css('height','1rem').animate({width:'90%'},500);
+                $('.big_img').animate({height:body_width*0.8},500);
+                
+                //显示文字标题
+                $('.img_info').animate({height:"3rem"},500);
+                $('.img_info .title').text(big_img_title);
+                
+            });
+
+            //关闭蒙版
+            $('.close').click(function () {
+                $('.hoverBox_show').hide();
+            });
+        });
+    </script>
 @stop
