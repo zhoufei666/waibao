4b7a6e36f799cd9ab9a4809b8a6458c6c374f4c2
zhoufei <596740325@qq.com>
2017-06-13 15:16:46 +0800

    Signed-off-by: zhoufei <596740325@qq.com>

--- resources/views/Public/Home/header_ad.blade.php
+++ resources/views/Public/Home/header_ad.blade.php
@@ -7,7 +7,7 @@
 
 <div class="introduce">
     <div class="container">
-        <h3 class="focusbox-title">你的大片能赚钱  飞毛腿-摄影 </h3>
-        <div class="focusbox-text"> 欣赏世界名家流作 提升你的生活水平 这是一个能赚钱的摄影平台 </div>
+        <h3 class="focusbox-title"> 欣赏世界名家流作 </h3>
+        <div class="focusbox-text">  培养兴趣爱好  提升生活质量  </div>
     </div>
 </div>
